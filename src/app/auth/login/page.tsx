eeb12f85af9d58b7f4c7331a25b5037024e26c4c
Vimalraj S <vimalraj.s@hepl.com>
2025-09-19 13:54:33 +0530

    UI changes done.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -329,12 +329,8 @@ const AuthPage = () => {
                   }
                   label="Remember me"
                 />
-                <Link
-                  href="#"
-                  underline="hover"
-                  sx={{ fontSize: "0.9rem", color: "#14ab78" }}
-                >
-                  Forgot?
+                <Link href="#" underline="hover" sx={{ fontSize: "0.9rem", color: "#14ab78" }}>
+                Forgot?
                 </Link>
               </Box>
             )}
